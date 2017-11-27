69042345b7a3a7f32d0f2c93568995b9fdc2aa65
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-11-27 16:32:14 -0500

    problem 4

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
 public class Problem4 {
+
+    public String canFormPalindrome(String str) {
+        String result;
+        int[] frequency = new int[300]; 
+        char[] strToCharArray = str.toCharArray();
+        for (char ch : strToCharArray) {
+            frequency[ch]++;
+        }
+
+        int oddCharCount = 0;
+        for (int ch : frequency) {
+            if (ch % 2 != 0) { // Count characters with odd occurrence.
+                oddCharCount++;
+            }
+            if (oddCharCount > 1) {// If more than one character in the string has odd occurrence then palindrome cannot be formed
+                result = "NO";
+                return result;
+            }
+        }
+
+        result = "YES";
+        return result;
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,29 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    Problem4 prob4Test;
+    @Before
+    public void setUp() {
+        prob4Test = new Problem4();
+    }
+
+    @Test
+    public void yesCanFormPalindrome() {
+        String expected = "YES";
+        String actual = prob4Test.canFormPalindrome("cdcdcdcdeeeef");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noCannotFormPalindrome() {
+        String expected = "NO";
+        String actual = prob4Test.canFormPalindrome("cdefghmnopqrstuvw");
+        Assert.assertEquals(expected, actual);
+    }
+
 }
